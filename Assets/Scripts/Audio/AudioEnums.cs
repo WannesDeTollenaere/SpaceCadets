5a8bb7aa370755f86e69fbec16566ab5f8646b0f
BramD98 <arcticbearodyssey@gmail.com>
2026-03-31 16:59:06 +0200

    Added extra MLA + Dist script

--- Assets/Scripts/Audio/AudioEnums.cs
+++ Assets/Scripts/Audio/AudioEnums.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
 
 namespace SpaceCadets.Audio
-{ 
-        public enum LilGuyElements {ScanStart,ScanLoop,ScanEnd,StoneFootstep,HackStart,HackLoop,HackEnd}
-        public enum RobotElements {Footstep,Idling,Throw }
+{
+    public enum LilGuyElements { ScanStart, ScanLoop, ScanEnd, StoneFootstep, HackStart, HackLoop, HackEnd }
+    public enum RobotElements { Footstep, Idling, Throw }
+    public enum EnvironmentElements { BombExplode }
+    public enum MusicElements {Percussion, Drums}
 }
