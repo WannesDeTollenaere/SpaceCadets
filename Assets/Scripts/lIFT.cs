2ab40c301b7012494c0e79730dc9911771ea9db2
AlexDS-GD29 <alexander.de.smet@student.howest.be>
2026-03-31 17:30:48 +0200

    remade elevator logic

--- Assets/Scripts/lIFT.cs
+++ Assets/Scripts/lIFT.cs
@@ -5,15 +5,33 @@ public class Lift : MonoBehaviour
 {
 
     [SerializeField]
-    private Animator _ElevatorAnim;
+    private Transform _ElevatorAnim;
+    [SerializeField]
+    private float _ExtendSpeed;
+
+    private bool _IsExtended;
 
     public void Activate()
     {
-        _ElevatorAnim.SetBool("IsElevated", true);
+        _IsExtended = true;
     }
 
     public void Deactivate()
     {
-        _ElevatorAnim.SetBool("IsElevated", false);
+        _IsExtended = false;
+    }
+
+    private void Update()
+    {
+        if (_ElevatorAnim == null) return;
+
+        if (_IsExtended)
+        {
+            Debug.Log("ae;ioja;difoj");
+            _ElevatorAnim.localPosition = Vector3.Slerp(_ElevatorAnim.localPosition, Vector3.up, _ExtendSpeed);
+        } else
+        {
+            _ElevatorAnim.localPosition = Vector3.Slerp(_ElevatorAnim.localPosition, Vector3.zero, _ExtendSpeed);
+        }
     }
 }
