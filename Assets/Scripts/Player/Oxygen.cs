c444dc3b286c1c98b80386f75cb7acb5cacdb9b0
Stan Olivier <104191739+Stoan24@users.noreply.github.com>
2026-04-01 13:56:34 +0200

    Player pulses when out of range

--- Assets/Scripts/Player/Oxygen.cs
+++ Assets/Scripts/Player/Oxygen.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class Oxygen : MonoBehaviour
 {
     [SerializeField] private float _drainRate = 0.3f;
+    [SerializeField] private PlayerJoin _playerJoin;
+    [SerializeField] private Material _normalMaterial;
+    [SerializeField] private Material _pulsingMaterial;
 
     private PlayerManager _playermanager;
-    private PlayerJoin _playerJoin;
 
     private float _currentMeter = 1f;
     private float _maxMeter = 1f;
@@ -13,10 +15,14 @@ public class Oxygen : MonoBehaviour
     public float CurrentOxygen => _currentMeter;
     public float MaxOxygen => _maxMeter;
 
+    private Renderer _renderer;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _playermanager = GetComponent<PlayerManager>();
+        _renderer = _playerJoin.SmallPlayer.GetComponentInChildren<Renderer>();
+
     }
 
     // Update is called once per frame
@@ -25,15 +31,28 @@ public class Oxygen : MonoBehaviour
         if (!_playermanager.ArePlayersInRange())
         {
             _currentMeter = Mathf.Max(0f, _currentMeter - _drainRate * Time.fixedDeltaTime);
+
+            if (_renderer.material != _pulsingMaterial)
+            {
+                _renderer.material = _pulsingMaterial;
+            }
         }
         else
         {
             _currentMeter = _maxMeter;
-        }
 
-        if (_currentMeter <= 0)
-        {
-            Destroy(_playerJoin.SmallPlayer);
+            if (_renderer.material != _normalMaterial)
+            {
+                _renderer.material = _normalMaterial;
+            }
+
+            if (_currentMeter <= 0)
+            {
+                if (_playerJoin.SmallPlayer)
+                {
+                    Destroy(_playerJoin.SmallPlayer);
+                }
+            }
         }
     }
 }
