5c25c3f9d73989d2110cdc48f93879d46e94607b
BramD98 <arcticbearodyssey@gmail.com>
2026-04-01 17:49:44 +0200

    Implented big G walk and idle anim sounds

---
+++ Assets/Scripts/Audio/BigGAnimSounds.cs
@@ -0,0 +1,22 @@
+using SpaceCadets.Audio;
+using UnityEngine;
+
+public class BigGAnimSounds : MonoBehaviour
+{
+    [SerializeField] private MultiLayerAudioRobot m_bigGMLA;
+    [SerializeField] private AudioSource m_oneshotSource;
+    [SerializeField] private AudioSource m_footstepSource;
+
+
+    private void PlayBigGIdleSound()
+    {
+        m_bigGMLA.PlayContainerElement(m_oneshotSource, RobotElements.Idling);
+        Debug.Log("Play idle");
+    }
+
+    private void PlayBGFootstepSound()
+    {
+        m_bigGMLA.PlayContainerElement(m_footstepSource, RobotElements.Footstep);
+        Debug.Log("Play footstep");
+    }
+}
