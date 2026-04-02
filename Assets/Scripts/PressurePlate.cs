3830e28180ee2ee989bc6b9e2960d93eca756308
WannesDT <wannes.de.tollenaere@student.howest.be>
2026-04-02 09:47:10 +0200

    Quick and dirty fix for pressureplate

--- Assets/Scripts/PressurePlate.cs
+++ Assets/Scripts/PressurePlate.cs
@@ -1,11 +1,11 @@
 using SpaceCadets.Audio;
 using UnityEngine;
-using UnityEngine.Events; 
+using UnityEngine.Events;
+using System.Collections.Generic;
 
 public class PressurePlate : MonoBehaviour
 {
-
-    [SerializeField] 
+    [SerializeField]
     private string _triggerTag = "Player";
     [SerializeField] private MultiLayerAudioEnvironment m_EnvMLA;
     private AudioSource m_audioSource;
@@ -18,36 +18,29 @@ public class PressurePlate : MonoBehaviour
     public UnityEvent OnReleased;
 
     private bool _IsDown;
-
-    private int _objectsOnPlate = 0;
+    
+    private List<Collider> _collidersOnPlate = new List<Collider>();
 
     private void Awake()
     {
         m_audioSource = GetComponent<AudioSource>();
     }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(_triggerTag))
         {
-            _objectsOnPlate++;
-
-            if (_objectsOnPlate == 1)
+            if (!_collidersOnPlate.Contains(other))
             {
-                OnPressed?.Invoke();
-                m_EnvMLA.PlayContainerElement(m_audioSource, EnvironmentElements.PressurePlateDown);
-                _IsDown = true;
-            }
-        }
-    }
+                _collidersOnPlate.Add(other);
 
-    private void Update()
-    {
-        if (_IsDown)
-        {
-            _VisualPosition.localPosition = Vector3.Slerp(_VisualPosition.localPosition, Vector3.down * 0.1f, flipDownSpeed * Time.deltaTime);
-        }
-        else if (!_IsDown) {
-            _VisualPosition.localPosition = Vector3.Slerp(_VisualPosition.localPosition, Vector3.zero, flipDownSpeed * Time.deltaTime);
+                if (_collidersOnPlate.Count == 1 && !_IsDown)
+                {
+                    OnPressed?.Invoke();
+                    m_EnvMLA.PlayContainerElement(m_audioSource, EnvironmentElements.PressurePlateDown);
+                    _IsDown = true;
+                }
+            }
         }
     }
 
@@ -55,15 +48,50 @@ public class PressurePlate : MonoBehaviour
     {
         if (other.CompareTag(_triggerTag))
         {
-            _objectsOnPlate--;
-
-            if (_objectsOnPlate <= 0)
+            if (_collidersOnPlate.Contains(other))
             {
-                _objectsOnPlate = 0; 
-                OnReleased?.Invoke();
-                m_EnvMLA.PlayContainerElement(m_audioSource, EnvironmentElements.PressurePlateUp);
-                _IsDown = false;
+                _collidersOnPlate.Remove(other);
+                CheckIfPlateShouldRelease();
             }
+        }
+    }
+
+    private void Update()
+    {
+        bool collidersRemoved = false;
+
+        for (int i = _collidersOnPlate.Count - 1; i >= 0; i--)
+        {
+            Collider col = _collidersOnPlate[i];
+            if (col == null || !col.enabled || !col.gameObject.activeInHierarchy)
+            {
+                _collidersOnPlate.RemoveAt(i);
+                collidersRemoved = true;
+            }
+        }
+
+        if (collidersRemoved)
+        {
+            CheckIfPlateShouldRelease();
+        }
+
+        if (_IsDown)
+        {
+            _VisualPosition.localPosition = Vector3.Slerp(_VisualPosition.localPosition, Vector3.down * 0.1f, flipDownSpeed * Time.deltaTime);
+        }
+        else
+        {
+            _VisualPosition.localPosition = Vector3.Slerp(_VisualPosition.localPosition, Vector3.zero, flipDownSpeed * Time.deltaTime);
+        }
+    }
+
+    private void CheckIfPlateShouldRelease()
+    {
+        if (_collidersOnPlate.Count == 0 && _IsDown)
+        {
+            OnReleased?.Invoke();
+            m_EnvMLA.PlayContainerElement(m_audioSource, EnvironmentElements.PressurePlateUp);
+            _IsDown = false;
         }
     }
 }
